f106b043946264af7b2b706d9280fb795662a32b
Aishaml1 <Aisha.lugg1@icloud.com>
2021-11-16 14:34:16 -0500

    solved 2

--- python-functions.py
+++ python-functions.py
@@ -16,3 +16,11 @@ print(sum_to(10))
 #Write a function named largest that takes a list of numbers as an argument and 
 #returns the largest number in that list.
 #max() function => returns largest number 
+def largest(li):
+    max = li[0]
+    for x in li:
+        if x > max:
+            max = x
+    return max
+print(largest([1, 2, 3, 4, 0])) # returns 4
+print (largest([10, 4, 2, 231, 91, 54]))  # returns 231
