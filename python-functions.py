d8f398f28d257ae3af6e753207455fce080f36da
Aishaml1 <Aisha.lugg1@icloud.com>
2021-11-16 14:27:39 -0500

    solved 1

--- python-functions.py
+++ python-functions.py
@@ -5,14 +5,13 @@
 
 def sum_to(n):
   sum = 0
-  for i in range(n+1):
+  for i in range(1, n + 1):
     sum+=i
   return sum
 
 print(sum_to(6)) 
 print(sum_to(10)) 
-print(sum_to(11)) 
-print(sum_to(12)) 
+
 
 #Write a function named largest that takes a list of numbers as an argument and 
 #returns the largest number in that list.
